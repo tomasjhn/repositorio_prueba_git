b452cecd6c46970962a6603eb462e9881daa75cd
tomas <tomasjhn@educastur.org>
2022-01-18 13:48:35 +0100

    Revert "confirmada modificación en Principal"
    This reverts commit 2c9dee1e731e4065749d00540478a361ab1db8b1.

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Ejemplo de código Java");
 	}
 
 }
