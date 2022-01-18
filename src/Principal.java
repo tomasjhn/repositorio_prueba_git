2c9dee1e731e4065749d00540478a361ab1db8b1
tomas <tomasjhn@educastur.org>
2022-01-18 13:08:42 +0100

    confirmada modificación en Principal

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Ejemplo de código Java");
 	}
 
 }
